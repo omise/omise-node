b0330cb57a380f0d9d23cc97c045301e54c80eca
zdk <di@omise.co>
2015-02-19 10:11:04 +0700

    Add update customer example

---
+++ examples/update_customer.js
@@ -0,0 +1,13 @@
+var config = {
+    'publicKey': process.env.OMISE_PUBLIC_KEY,
+    'secretKey': process.env.OMISE_SECRET_KEY
+};
+
+var omise = require('omise-node')(config);
+
+
+//update
+var customerId = 'cust_test_4z2owmajzsb3c527wj7';
+omise.customers.update(customerId, {description: 'the other description'}, function(err, resp) {
+  console.log(resp);
+});
