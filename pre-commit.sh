c731d6d102ae6e6641a146b4bc49fbc6b8c8f737
zdk <di@omise.co>
2015-02-19 13:02:51 +0700

    Fix git pre-commit script and relevant style

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,22 @@
+#!/bin/sh
 git stash -q --keep-index
+
+#check code styling
 npm run jscs
+RESULT=$?
+git stash pop -q
+if [ $RESULT -ne 0 ];
+then
+    echo "Inconsistent style, commit has failed."
+    exit 1
+fi
+#run test
 npm test
 RESULT=$?
 git stash pop -q
-[ $RESULT -ne 0 ] && exit 1
+if [ $RESULT -ne 0 ];
+then
+  echo "Test failed, commit has failed."
+  exit 1
+fi
 exit 0
