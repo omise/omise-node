5906d2796e973dde18e400373053f2aafe2a535c
zdk <di@omise.co>
2015-02-19 17:11:34 +0700

    Continue refactoring as needed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 //Omise.co
-var omiseResources = require('./lib/apiResources.js');
+var resource = require('./lib/apiResources.js');
 module.exports = function(config) {
-  return omiseResources(config);
+  return resource.omiseResources(config);
 };
