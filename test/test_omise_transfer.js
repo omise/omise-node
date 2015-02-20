9e91cbcf5bbb269e6ea82008dd32d7c495dc61f5
zdk <di@omise.co>
2015-02-20 16:58:24 +0700

    Add a feature to retrive a transfer

--- test/test_omise_transfer.js
+++ test/test_omise_transfer.js
@@ -22,42 +22,15 @@ describe('Omise', function() {
       });
     });
 
-    //it('should be able to list all transfers', function(done) {
-      //testHelper.setupMock('transfers_list');
-      //omise.transfers.list(function(err, resp) {
-        //expect(resp.object, 'transfer');
-      //});
-      //done();
-    //});
-
-    //it('should be able to retrieve an existing transfer', function(done) {
-      //testHelper.setupMock('transfer_retrieve');
-      //var transferId = 'cust_test_4z33o46lqreryhqua8w';
-      //omise.transfers.retrieve(transferId, function(err, resp) {
-        //expect(resp.object, 'list');
-        //expect(resp.data).to.be.instanceof(Array);
-      //});
-      //done();
-    //});
-
-    //it('should be able to destroy an existing transfer', function(done) {
-      //testHelper.setupMock('transfer_delete');
-      //var transferId = 'cust_test_4yygdeiu4ko863sxts9';
-      //omise.transfers.destroy(transferId, function(err, resp) {
-        //expect(resp.object, 'transfer');
-        //expect(resp.deleted).to.be.true;
-      //});
-      //done();
-    //});
-
-    //it('should be able to update an existing transfer', function(done) {
-      //testHelper.setupMock('transfer_update');
-      //var transferId = 'cust_test_4z2owmajzsb3c527wj7';
-      //omise.transfers.update(transferId, function(err, resp) {
-        //expect(resp.object, 'transfer');
-        //expect(resp.description, 'New description');
-      //});
-      //done();
-    //});
+    it('should be able to retrieve an existing transfer', function(done) {
+      testHelper.setupMock('transfers_retrieve');
+      var transferId = 'trsf_test_4z4cw3ku87retguciji';
+      omise.transfers.retrieve(transferId, function(err, resp) {
+        expect(resp.object, 'transfer');
+        var amount = resp.amount;
+        amount.should.equal(4000);
+      });
+      done();
+    });
   })
 })
