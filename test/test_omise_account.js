b5b9632bc59e6ec57f7c19ce19988973b3814604
zdk <di@omise.co>
2015-02-23 15:15:50 +0700

    Add a support to retrieve an account

---
+++ test/test_omise_account.js
@@ -0,0 +1,21 @@
+var chai   = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+var config = require('./config.js');
+var omise  = require('../index')(config);
+var testHelper = require('./testHelper');
+
+describe('Omise', function() {
+  describe('#account', function() {
+    it('should be able to retrieve an account', function(done) {
+      testHelper.setupMock('account_retrieve');
+      omise.account.retrieve(function(err, resp) {
+        expect(resp.object, 'account');
+        expect(resp.id, 'acct_123');
+        expect(resp.email, 'test@omise.co');
+      });
+      done();
+    });
+  })
+})
