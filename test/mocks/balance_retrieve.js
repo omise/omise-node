3563b7909ad0701bf76c16d5e3f6cecf76f9505d
zdk <di@omise.co>
2015-02-23 15:40:07 +0700

    Add a support to retrieve a balance

---
+++ test/mocks/balance_retrieve.js
@@ -0,0 +1,10 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .get('/balance')
+            .reply(200, { "object": "balance",
+                          "livemode": false,
+                          "available": 97242,
+                          "total": 385524,
+                          "currency": "thb"
+                        }, { server: 'nginx/1.1', 'content-type': 'application/json', } );
