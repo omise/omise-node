43908448424b7dce3276b0a7a16d81adb599f34d
zdk <di@omise.co>
2015-02-23 15:15:50 +0700

    Add a support to retrieve an account

---
+++ test/mocks/account_retrieve.js
@@ -0,0 +1,10 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .get('/account')
+            .reply(200, {
+              "object": "account",
+              "id": "acct_123",
+              "email": "test@omise.co",
+              "created": "2015-02-02T13:19:17Z"
+            }, { server: 'nginx/1.1', 'content-type': 'application/json', } );
