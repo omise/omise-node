b75e04c9d06b93d85fda5bd6c9ae70bac0d69847
zdk <di@omise.co>
2015-02-24 18:07:59 +0700

    Add support to create a refund

---
+++ test/mocks/refunds_create.js
@@ -0,0 +1,15 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .post('/charges/chrg_test_4z4295deewwyaviw003/refunds')
+            .reply(200, { "object": "refund",
+                          "id": "rfnd_test_4z5xxgntg3pzhhynae4",
+                          "location": "/charges/chrg_test_4z4295deewwyaviw003/refunds/rfnd_test_4z5xxgntg3pzhhynae4",
+                          "amount": 100000,
+                          "currency": "thb",
+                          "charge": "chrg_test_4z4295deewwyaviw003",
+                          "transaction": "trxn_test_4z5xxgnzst7caoa8f79",
+                          "created": "2015-02-24T10:39:26Z"
+                        }, {server: 'nginx/1.1', 'content-type': 'application/json'});
+
+
