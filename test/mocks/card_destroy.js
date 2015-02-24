7821e1b263a85fa7757c5e21f457aa27a22c3bc5
zdk <di@omise.co>
2015-02-24 15:17:38 +0700

    Add support to destroy a card

---
+++ test/mocks/card_destroy.js
@@ -0,0 +1,11 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .intercept('/customers/cust_test_4z2owmajzsb3c527wj7/cards/card_test_4z2owrdmvbygi7ah0fu', 'DELETE')
+            .reply(200, { "object": "card",
+                          "id": "card_test_4z2owrdmvbygi7ah0fu",
+                          "livemode": false,
+                          "deleted": true
+                         },
+                      { server: 'nginx/1.1', 'content-type': 'application/json' }
+                  );
