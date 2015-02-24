7eb47b75897b3294e91332c0adba7d77c276adc7
zdk <di@omise.co>
2015-02-24 12:07:03 +0700

    Add suppot to retrieve a card and a bunch of refactoring

--- test/test_omise_cards.js
+++ test/test_omise_cards.js
@@ -15,8 +15,20 @@ describe('Omise', function() {
         expect(resp.object, 'list');
         expect(resp.data).to.be.instanceof(Array);
         expect(resp.data[0].object, 'card');
+        done();
       });
-      done();
+    });
+
+    it('should be able to retrieve a card', function(done) {
+      testHelper.setupMock('card_retrieve');
+      var customerId = 'cust_test_4z2owmajzsb3c527wj7';
+      var cardId = 'card_test_4z2owrdmvbygi7ah0fu';
+      omise.customers.retrieveCard(customerId, cardId, function(err, resp) {
+        expect(resp.object, 'card');
+        expect(resp.id, 'card_test_4z2owrdmvbygi7ah0fu');
+        expect(resp.brand, 'Visa');
+        done();
+      });
     });
   })
 })
