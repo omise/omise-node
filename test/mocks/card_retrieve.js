3668dc5bf7a960a2ce129e3dc6809767b62e979f
zdk <di@omise.co>
2015-02-24 12:07:03 +0700

    Add suppot to retrieve a card and a bunch of refactoring

---
+++ test/mocks/card_retrieve.js
@@ -0,0 +1,21 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .get('/customers/cust_test_4z2owmajzsb3c527wj7/cards/card_test_4z2owrdmvbygi7ah0fu')
+            .reply(200, { "object": "card",
+                          "id": "card_test_4z2owrdmvbygi7ah0fu",
+                          "livemode": false,
+                          "location": "/customers/cust_test_4z2owmajzsb3c527wj7/cards/card_test_4z2owrdmvbygi7ah0fu",
+                          "country": "us",
+                          "city": "Bangkok",
+                          "postal_code": "10320",
+                          "financing": "",
+                          "last_digits": "4242",
+                          "brand": "Visa",
+                          "expiration_month": 2,
+                          "expiration_year": 2017,
+                          "fingerprint": "umrBpbHRuc8vstbcNEZPbnKkIycR/gvI6ivW9AshKCw=",
+                          "name": "JOHN DOE",
+                          "security_code_check": true,
+                          "created": "2015-02-16T03:12:10Z"
+                        }, { server: 'nginx/1.1', 'content-type': 'application/json', });
