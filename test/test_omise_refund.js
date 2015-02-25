eafb97c3b11c58e40461ab63a44fc2e7c1a6fe38
zdk <di@omise.co>
2015-02-25 12:15:08 +0700

    Add support to retrieve a refund and refactor

--- test/test_omise_refund.js
+++ test/test_omise_refund.js
@@ -31,5 +31,19 @@ describe('Omise', function() {
         done();
       });
     });
+
+    it('should be able to retrieve a refund', function(done) {
+      testHelper.setupMock('refunds_retrieve');
+      var chargeId = 'chrg_test_4z4295deewwyaviw003';
+      var refundId = 'rfnd_test_4z5xxgntg3pzhhynae4';
+      omise.charges.retrieveRefund(chargeId, refundId, function(err, resp) {
+        expect(resp.object, 'refund');
+        expect(resp.id, 'rfnd_test_4z5xxgntg3pzhhynae4');
+        expect(resp.charge, 'chrg_test_4z4295deewwyaviw003');
+        expect(resp.transaction, 'trxn_test_4z5xxgnzst7caoa8f79');
+        done();
+      });
+    });
+
   })
 })
