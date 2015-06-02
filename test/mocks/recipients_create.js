efb7bab093bf00e15920e1dde985d49301116323
zdk <di@omise.co>
2015-06-02 11:55:40 +0700

    Add support for Add a recipient operation.

---
+++ test/mocks/recipients_create.js
@@ -0,0 +1,25 @@
+var nock  = require('nock');
+var scope = nock('https://api.omise.co')
+            .persist()
+            .post('/recipients')
+            .reply(200, { "object": "recipient",
+                          "id": "recp_test_5086be32wsp9iy2exem",
+                          "livemode": false,
+                          "location": "/recipients/recp_test_5086be32wsp9iy2exem",
+                          "verified": false,
+                          "active": false,
+                          "name": "John Doe",
+                          "email": "john.doe@example.com",
+                          "description": "John Doe (id: 30)",
+                          "type": "individual",
+                          "tax_id": "1234567890",
+                          "bank_account": {
+                            "object": "bank_account",
+                            "brand": "bbl",
+                            "last_digits": "7890",
+                            "name": "John Doe",
+                            "created": "2015-06-02T04:38:42Z"
+                          },
+                          "failure_code": null,
+                          "created": "2015-06-02T04:38:42Z"
+                        }, { server: 'nginx/1.1', 'content-type': 'application/json', });
--- test/test_omise_recipients.js
+++ test/test_omise_recipients.js
@@ -9,6 +9,30 @@ var testHelper = require('./testHelper');
 
 describe('Omise', function() {
   describe('#Recipients', function() {
+    it('should be able to create recipient', function(done) {
+      var recipient = {
+        'name': 'John Doe',
+        'description': 'John Doe (id: 30)',
+        'email': 'john.doe@example.com',
+        'type': 'individual',
+        'tax_id': 1234567890,
+        'bank_account': {
+          'brand': 'bbl',
+          'number': '1234567890',
+          'name': 'John Doe'
+        }
+      };
+      testHelper.setupMock('recipients_create');
+      omise.recipients.create(recipient, function(err, resp) {
+        expect(resp.object, 'recipient');
+        expect(resp.type, 'individual');
+        expect(resp.type, 1273456789);
+        expect(resp).to.include.keys('bank_account');
+        expect(resp.bank_account.last_digits, 7890);
+        done();
+      });
+    });
+
     it('should be able to list all recipients', function(done) {
       testHelper.setupMock('recipients_list');
       omise.recipients.list(function(err, resp) {
