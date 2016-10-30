d28414eac64c0be5fd3f9ef7f7449169082c29ec
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-10-30 14:48:50 +0700

    - add failing test case - fix coding style in test_omise_events.js

--- test/test_omise_customers.js
+++ test/test_omise_customers.js
@@ -53,6 +53,66 @@ describe('Omise', function() {
       });
     });
 
+    it('should be able to create and capture charge using customer ID',
+      function(done) {
+        testHelper.setupMock('charges_create');
+        var charge = { 'description': 'Charge for order 3948',
+                       'amount': '100000',
+                       'currency': 'thb',
+                       'capture': false,
+                       'customer': customerId };
+        omise.charges.create(charge, function(err, resp) {
+          expect(resp.object, 'charge');
+          var chargeId = resp.id;
+          expect(chargeId).to.match(/^chrg_test/);
+          expect(resp.capture).be.false;
+          expect(resp.paid).be.false;
+          done();
+        });
+      }
+    );
+
+    it('should be able to create and capture charge again using token',
+      function(done) {
+        testHelper.setupMock('tokens_create');
+        var cardDetails = {
+          'card':{
+            'name': 'JOHN DOE',
+            'city': 'Bangkok',
+            'postal_code': 10320,
+            'number': '4242424242424242',
+            'expiration_month': 2,
+            'expiration_year': 2017,
+            'security_code': 123
+          }
+        };
+        omise.tokens.create(cardDetails, function(err, resp) {
+          should.exist(resp.id);
+          tokenId = resp.id;
+          expect(tokenId).to.contains('tokn_test');
+          should.exist(resp.card.id);
+          var cardId  = resp.card.id;
+          expect(cardId).to.contains('card_test');
+          testHelper.setupMock('charges_create');
+
+          var charge = { 'description': 'Charge for order 3949',
+                         'amount': '100000',
+                         'currency': 'thb',
+                         'capture': false,
+                         'card': tokenId };
+          omise.charges.create(charge, function(err, resp) {
+            expect(err).to.equal(null, err !== null ? err.message : null);
+            expect(resp.object, 'charge');
+            var chargeId = resp.id;
+            expect(chargeId).to.match(/^chrg_test/);
+            expect(resp.capture).be.false;
+            expect(resp.paid).be.false;
+            done();
+          });
+        });
+      }
+    );
+
     it('should be able to list all customers', function(done) {
       testHelper.setupMock('customers_list');
       omise.customers.list(function(err, resp) {
