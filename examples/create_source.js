1f197bd151df4310f0760a08f88b56423ea5b383
Bhoomtawath Plinsut <plinsut@omise.co>
2017-12-20 13:14:43 +0700

    Add an example of a charging with a source

--- examples/create_source.js
+++ examples/create_source.js
@@ -1,21 +1,28 @@
 'use strict';
 
+exports.__esModule = true;
+var amount = 500000;
+var currency = 'thb';
 var omise = require('../index')({
-  'publicKey': 'pkey_test_597dvdthp1ax5xyir80',
+  'publicKey': process.env.OMISE_PUBLIC_KEY,
   'secretKey': process.env.OMISE_SECRET_KEY,
 });
 
 var source = {
-  type:              'installment_kbank',
-  amount:            '500000',
-  currency:          'thb',
-  installment_terms: '4',
+  'type':     'internet_banking_bbl',
+  'amount':   500000,
+  'currency': 'thb',
 };
 
-omise.sources.create(source, function(err, resp) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(resp);
+omise.sources.create(source).then(function(resSource) {
+  return omise.charges.create({
+    'amount':     amount,
+    'source':     resSource.id,
+    'currency':   currency,
+    'return_uri': 'https://omise.co',
+  });
+}).then(function(charge) {
+  console.log(charge);
+}).catch(function(err) {
+  console.log(err);
 });
