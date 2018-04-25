a620ba9ebfd532c9c43c021352df938cefc2f669
Bhoomtawath Plinsut <plinsut@omise.co>
2018-04-25 18:32:17 +0700

    Add schedules endpoint for Customers' type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -184,6 +184,7 @@ declare namespace Omise {
       updateCard(customerID: string, cardID: string, details: Cards.ICardRequest,
                  callback?: ResponseCallback<Cards.ICard>): Bluebird<Cards.ICard>;
       destroyCard(customerID: string, cardID: string, callback?: ResponseCallback<Cards.ICard>): Bluebird<Cards.ICard>;
+      schedules(customerID: string, callback?: ResponseCallback<Schedules.IChargeScheduleList>): Bluebird<Schedules.IChargeScheduleList>;
     }
 
     interface IRequest {
