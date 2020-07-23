63035953c62186afe543f4fe64315545be6fdd9b
Rezigned <rezigned@users.noreply.github.com>
2020-07-23 10:52:06 +0700

    Enforce type checking during CI build (#135)
    * Enforce type checking during CI build
    
    * Add IRefundRequest type
    
    * Update release notes

--- types/index.d.ts
+++ types/index.d.ts
@@ -53,6 +53,14 @@ declare namespace Omise {
       country: string;
       zero_interest_installments: boolean;
     }
+
+    interface IRequest {
+      chain_enabled?: boolean;
+      chain_return_uri?: string;
+      metadata_export_keys?: object;
+      webhook_uri?: string;
+      zero_interest_installments?: boolean;
+    }
   }
 
   export namespace Balance {
@@ -119,7 +127,7 @@ declare namespace Omise {
       capture(chargeID: string, callback?: ResponseCallback<ICharge>): Bluebird<ICharge>;
       reverse(chargeID: string, callback?: ResponseCallback<ICharge>): Bluebird<ICharge>;
       expire(chargeID: string, callback?: ResponseCallback<ICharge>): Bluebird<ICharge>;
-      createRefund(chargeID: string, callback?: ResponseCallback<IRefundResponse>): Bluebird<IRefundResponse>;
+      createRefund(chargeID: string, req: IRefundRequest, callback?: ResponseCallback<IRefundResponse>): Bluebird<IRefundResponse>;
       listRefunds(chargeID: string, callback?: ResponseCallback<IListRefundResponse>): Bluebird<IListRefundResponse>;
       retrieveRefund(chargeID: string, refundID: string, callback?: ResponseCallback<IRefundResponse>): Bluebird<IRefundResponse>;
     }
@@ -166,6 +174,12 @@ declare namespace Omise {
       data: ICharge[];
     }
 
+    interface IRefundRequest {
+      amount: number;
+      metadata?: { [key: string]: any };
+      void?: boolean
+    }
+
     interface IRefundResponse extends IBaseResponse {
       amount: number;
       currency: string;
@@ -173,6 +187,7 @@ declare namespace Omise {
       transaction: string;
       created: string;
       voided: boolean;
+      metadata?: { [key: string]: any };
     }
   }
 
