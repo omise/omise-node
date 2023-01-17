52a819fca4d224b0d1e1dcacd6262ca413684c0e
Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>
2023-01-17 15:25:01 +0700

    [ENGA3-808] : added unit test for resourceActions

---
+++ test/test_omise_api_resource.js
@@ -0,0 +1,34 @@
+var chai = require('chai');
+var expect = chai.expect;
+var resource = require('../lib/apiResources')
+
+describe('Api Resource', function () {
+  describe('#resourceActions', function () {
+    it('result must have property retrieve', function (done) {
+      const result = resource.resourceActions('balance',
+        ['retrieve'],
+        {
+          host: 'api.omise.co',
+          key:  process.env.OMISE_SECRET_KEY,
+          omiseVersion: '2015-09-10',
+        }
+      );
+      expect(result).to.have.property('retrieve')
+      done()
+    });
+
+    it('result must have property create and retrieve', function (done) {
+      const result = resource.resourceActions('balance',
+        ['create', 'retrieve'],
+        {
+          host: 'api.omise.co',
+          key:  process.env.OMISE_SECRET_KEY,
+          omiseVersion: '2015-09-10',
+        }
+      );
+      expect(result).to.have.property('create')
+      expect(result).to.have.property('retrieve')
+      done()
+    });
+  });
+});
